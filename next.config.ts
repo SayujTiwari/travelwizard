b6cbbc23a31f280cc1332bc698f756991c1289c0
SayujTiwari <tiwarisayuj@gmail.com>
2025-09-03 11:57:53 -0600

    added feautre to add trips and images as well

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "d13ziab59z.ufs.sh"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
